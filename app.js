701586cf9b08c877aa4d82894c7dbc8f2a9b363d
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-04-28 21:59:00 +0200

    connecting to DB

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const connectDB = require('./db/connect');
+
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(PORT, () => {
       console.log(`Server is listening on port: ${PORT}`);
     })
