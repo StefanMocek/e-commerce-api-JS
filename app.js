e42c2b086a971434571c8e7be4c39fa226028caa
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-04-28 22:05:34 +0200

    error handling middlewares and home route

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
@@ -6,6 +7,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const connectDB = require('./db/connect');
+
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to e-commerce API')
+});
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
