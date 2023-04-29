06990adf8ec065f3fbd49a7a7dd540c816b21a6c
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-04-29 22:12:30 +0200

    authentication middleware setup

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -6,15 +6,16 @@ const {
   updateUser,
   updateUserPassword
 } = require('../controllers/user.Controller');
+const {authenticateUser} = require('../middleware/authentication');
 
 const router = express.Router();
 
-router.route('/').get(getAllUsers);
+router.route('/').get(authenticateUser, getAllUsers);
 
 router.route('/showMe').get(showCurrentUser);
 router.route('/updateUser').patch(updateUser);
 router.route('/updateUserPassword').patch(updateUserPassword);
 
-router.route('/:id').get(getSingleUser);
+router.route('/:id').get(authenticateUser, getSingleUser);
 
 module.exports = router;
