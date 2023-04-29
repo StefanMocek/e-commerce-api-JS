488e5f12bd1da6a0df9cb8fccc106a7463f9fa6f
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-04-29 13:05:30 +0200

    dest req.body to prevent adding admin role

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,13 +3,13 @@ const User = require('../models/user.model');
 const CustomError = require('../errors');
 
 const registerController = async (req, res) => {
-  const {email} = req.body;
+  const {name, email, password} = req.body;
   const emailValidator = await User.findOne({email});
   if(emailValidator) {
     throw new CustomError.BadRequestError('Wrong credentials')
   };
 
-  const user = await User.create(req.body);
+  const user = await User.create({name, email, password});
   res.status(StatusCodes.CREATED).json({user});
 };
 
