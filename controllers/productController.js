0202717b9d2d24e073c1177eee976dbc84c43c9d
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-04-30 15:17:42 +0200

    rest of product controllers

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,54 @@
+const {StatusCodes} = require('http-status-codes');
+const Product = require('../models/product.model');
+const CustomError = require('../errors');
+
 const createProduct = async (req, res) => {
-  res.send('createProduct')
+  req.body.user = req.user.userId;
+  const product = await Product.create(req.body)
+  res.status(StatusCodes.CREATED).json({product});
 };
 
 const getAllProducts = async (req, res) => {
-  res.send('getAllProducts')
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({products, count: products.length});
 };
 
 const getSingleProduct = async (req, res) => {
-  res.send('getSingleProduct')
+  const {id: productId} = req.params;
+  const product = await Product.findOne({_id: productId});
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${req.params.id}`);
+  };
+  res.status(StatusCodes.OK).json({product});
 };
 
 const updateProduct = async (req, res) => {
-  res.send('updateProduct')
+  const {id: productId} = req.params;
+  const product = await Product.findOneAndUpdate(
+    {_id: productId},
+    req.body,
+    {new: true, runValidators: true}
+  );
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${req.params.id}`);
+  };
+
+  res.status(StatusCodes.OK).json({product});
 };
 
 const deleteProduct = async (req, res) => {
-  res.send('deleteProduct')
+  const {id: productId} = req.params;
+  const product = await Product.findOne({_id: productId});
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${req.params.id}`);
+  };
+
+  await product.remove();
+  
+  res.status(StatusCodes.OK).json({msg: 'Success'});
 };
 
 const uploadImage = async (req, res) => {
