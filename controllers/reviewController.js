b15e36611a4bef20a65fa8051e641da33242b475
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-05-01 20:40:23 +0200

    createReview

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,9 +1,30 @@
 const {StatusCodes} = require('http-status-codes');
 const Review = require('../models/review.model');
+const Product = require('../models/product.model');
 const CustomError = require('../errors');
+const {checkPermissions} = require('../utils')
 
 const createReview = async (req, res) => {
-  res.send('createReview');
+  const {product: productId} = req.body;
+  const {userId} = req.user;
+
+  const isValidProduct = await Product.findOne({_id: productId});
+  if (!isValidProduct) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+  };
+
+  const alreadyReviewed = await Product.findOne({
+    product: productId,
+    user: userId
+  });
+
+  if (!alreadyReviewed) {
+    throw new CustomError.BadRequestError('Already reviewed ')
+  };
+
+  req.body.user = userId;
+  const review = Review.create(req.body)
+  res.status(StatusCodes.CREATED).json({review});
 };
 
 const getAllReviews = async (req, res) => {
