e8771c6d07f4c17071e70800c1c31d35b27fd9ed
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-05-01 19:27:05 +0200

    only one users review per product

--- models/review.model.js
+++ models/review.model.js
@@ -36,4 +36,6 @@ const reviewSchema = mongoose.Schema(
   {timestamps: true}
 );
 
+reviewSchema.index({product: 1, user: 1}, {unique: true});
+
 module.exports = mongoose.model('Review', reviewSchema);
