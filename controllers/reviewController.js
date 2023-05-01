eea4d661107c53ab875a5b298399cefd37fd696c
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-05-01 19:55:52 +0200

    reviews setup

---
+++ controllers/reviewController.js
@@ -0,0 +1,31 @@
+const {StatusCodes} = require('http-status-codes');
+const Review = require('../models/review.model');
+const CustomError = require('../errors');
+
+const createReview = async (req, res) => {
+  res.send('createReview');
+};
+
+const getAllReviews = async (req, res) => {
+  res.send('getAllReviews');
+};
+
+const getSingleReview = async (req, res) => {
+  res.send('getSingleReview');
+};
+
+const updateReview = async (req, res) => {
+  res.send('updateReview');
+};
+
+const deleteReview = async (req, res) => {
+  res.send('deleteReview');
+};
+
+module.exports = {
+  createReview,
+  getAllReviews,
+  getSingleReview,
+  updateReview,
+  deleteReview
+}
