3a24e817b87f28319eac5e155cff02c27dedef99
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-05-01 20:58:20 +0200

    updateReview

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -43,11 +43,36 @@ const getSingleReview = async (req, res) => {
 };
 
 const updateReview = async (req, res) => {
-  res.send('updateReview');
+  const {rating, title, comment} = req.body;
+  const {id: reviewId} = req.params;
+  const review = await Review.findOne({_id: reviewId});
+
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id: ${req.params.id}`);
+  };
+
+  checkPermissions(req.user, review.user);
+
+  review.rating = rating;
+  review.title = title;
+  review.comment = comment;
+
+  await review.save();
+  res.status(StatusCodes.OK).json({review});
 };
 
 const deleteReview = async (req, res) => {
-  res.send('deleteReview');
+  const {id: reviewId} = req.params;
+  const review = await Review.findOne({_id: reviewId});
+
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id: ${req.params.id}`);
+  };
+
+  checkPermissions(req.user, review.user);
+
+  await review.remove();
+  res.status(StatusCodes.OK).json({msg: 'Success'});
 };
 
 module.exports = {
