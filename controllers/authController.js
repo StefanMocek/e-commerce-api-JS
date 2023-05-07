04a73c60a5e3675187dd0a086c0a0582e182e64b
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-05-07 20:01:36 +0000

    register controllers - dont send the cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,11 @@
-const {StatusCodes} = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 const User = require('../models/user.model');
 const CustomError = require('../errors');
-const {attachCookiesToResponse, createTokenUser} = require('../utils')
+const { attachCookiesToResponse, createTokenUser } = require('../utils')
 
 const registerController = async (req, res) => {
-  const {name, email, password} = req.body;
-  const emailValidator = await User.findOne({email});
+  const { name, email, password } = req.body;
+  const emailValidator = await User.findOne({ email });
   if (emailValidator) {
     throw new CustomError.BadRequestError('Wrong credentials')
   };
@@ -14,34 +14,34 @@ const registerController = async (req, res) => {
   const isFirstAccount = await user.count({}) === 0;
   const role = isFirstAccount ? 'admin' : ' user';
 
-  const user = await User.create({name, email, password, role});
+  const verificationToken = 'temp fake token';
 
-  const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({res, user: tokenUser})
+  const user = await User.create({ name, email, password, role, verificationToken });
 
-  res.status(StatusCodes.CREATED).json({user: tokenUser});
+  // temoporary sending the verificationToken - juest for test in postman
+  res.status(StatusCodes.CREATED).json({ msg: 'success - chceck your email', verificationToken })
 };
 
 const loginController = async (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
   if (!email || !password) {
     throw new CustomError.BadRequestError('Please provide email and password')
   };
 
-  const user = await User.findOne({email});
+  const user = await User.findOne({ email });
   if (!user) {
     throw new CustomError.UnauthenticatedError('Wrong credentials')
   };
 
   const isPasswordCorrect = await user.comparePassword(password);
-  if(!isPasswordCorrect){
-    throw new CustomError.UnauthenticatedError('Wrong credentials')  
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError('Wrong credentials')
   };
 
   const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({res, user: tokenUser});
+  attachCookiesToResponse({ res, user: tokenUser });
 
-  res.status(StatusCodes.OK).json({user: tokenUser});
+  res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
 const logoutController = async (req, res) => {
@@ -49,7 +49,7 @@ const logoutController = async (req, res) => {
     httpOnly: true,
     expires: new Date(Date.now())
   })
-  res.status(StatusCodes.OK).send({message: 'user logout'})
+  res.status(StatusCodes.OK).send({ message: 'user logout' })
 };
 
 module.exports = {
